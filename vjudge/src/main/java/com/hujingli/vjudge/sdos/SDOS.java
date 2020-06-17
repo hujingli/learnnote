4d0b73cc42a40990abe9c69f17257f4fec638dbe
exphuhong <1042331526@qq.com>
2020-06-17 22:04:29 +0800

    add djstl min road

---
+++ vjudge/src/main/java/com/hujingli/vjudge/sdos/SDOS.java
@@ -0,0 +1,21 @@
+package com.hujingli.vjudge.sdos;
+
+import java.util.Scanner;
+
+/**
+ * 六度分离理论 six degrees of separation   需要用到floyd算法
+ */
+public class SDOS {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+        String[] split = in.split(" ");
+
+        int N = Integer.parseInt(split[0]);
+        int M = Integer.parseInt(split[1]);
+
+
+    }
+
+}
