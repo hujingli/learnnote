9b942a9a5c95b2f5e16cb22ea26294082d586bab
exphuhong <1042331526@qq.com>
2020-09-02 21:19:54 +0800

    添加抽象工厂

---
+++ design/src/main/java/com/hujingli/design/factory/abstractfactory/Car.java
@@ -0,0 +1,10 @@
+package com.hujingli.design.factory.abstractfactory;
+
+/**
+ * @author <a href="exphuhong@163.com">胡红</a>
+ * @Date 2020 年 09 月 01 日
+ * @Description
+ * @since
+ */
+public class Car {
+}
---
+++ design/src/main/java/com/hujingli/design/factory/simple/Car.java
@@ -0,0 +1,10 @@
+package com.hujingli.design.factory.simple;
+
+/**
+ * @author <a href="exphuhong@163.com">胡红</a>
+ * @Date 2020 年 08 月 31 日
+ * @Description
+ * @since
+ */
+public class Car {
+}
