97a4c60b9b88f49e6b89858ef24cf33bcb606a99
exphuhong <1042331526@qq.com>
2020-09-03 21:42:16 +0800

    添加装饰器模式

---
+++ design/src/main/java/com/hujingli/design/decorator/WomanDecorator.java
@@ -0,0 +1,10 @@
+package com.hujingli.design.decorator;
+
+/**
+ * @author <a href="exphuhong@163.com">胡红</a>
+ * @Date 2020 年 09 月 03 日
+ * @Description
+ * @since
+ */
+public class WomanDecorator {
+}
---
+++ design/src/test/java/Test.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @author <a href="exphuhong@163.com">胡红</a>
+ * @Date 2020 年 09 月 03 日
+ * @Description
+ * @since
+ */
+public class Test {
+}
