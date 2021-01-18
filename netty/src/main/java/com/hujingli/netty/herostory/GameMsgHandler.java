2266e42225a17115980c9a5816a2fcca44bf25f2
exphuhong <1042331526@qq.com>
2021-01-18 23:28:58 +0800

    add GameMsgProto

--- netty/src/main/java/com/hujingli/netty/herostory/GameMsgHandler.java
+++ netty/src/main/java/com/hujingli/netty/herostory/GameMsgHandler.java
@@ -1,6 +1,5 @@
 package com.hujingli.netty.herostory;
 
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -9,6 +8,6 @@ public class GameMsgHandler extends SimpleChannelInboundHandler<Object> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Object o) throws Exception {
         System.out.println(o);
-        ctx.writeAndFlush(Unpooled.copiedBuffer("hello".getBytes()));
+
     }
 }
